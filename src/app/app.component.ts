4f32006bb3081fea967e2ae63b528f9acff7da88
Kauê Cardoso <kauekauada@hotmail.com>
2024-11-01 23:14:03 -0300

    FrontFinal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BsDropdownModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [
+    { provide: BsDropdownConfig, useValue: { autoClose: true, isAnimated: true, display: 'static' } }
+  ]
 })
 export class AppComponent {
   title = 'frontend';
--- src/app/components/layout/dashboard/dashboard.component.ts
+++ src/app/components/layout/dashboard/dashboard.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MdbCarouselModule],
+  imports: [CarouselModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
 }
