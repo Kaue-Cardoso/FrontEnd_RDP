66eacd2134153baca991febd76aadba58533c6b3
Joshua Binotto <joshuabinotto2@gmail.com>
2024-11-21 22:17:03 -0300

    esqueci algo

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,17 +1,20 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { MdbDropdownDirective, MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
 import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
 import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { RouterOutlet } from '@angular/router';
+import { LoginService } from '../../../auth/login.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MdbDropdownModule, MdbRippleModule, MdbCollapseModule, RouterOutlet],
+  imports: [MdbDropdownModule, MdbRippleModule, MdbCollapseModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  loginService = inject(LoginService)
+
   @ViewChild('dropdown') dropdown!: MdbDropdownDirective;
 
   openDropdown() {
